841572ee9bfec55632f358564e6b45f89aa5602c
liruqi <liruqi@gmail.com>
2013-12-14 04:11:51 +0800

    Use dinic for max flow

--- Others/directed-bipartite-graph.cpp
+++ Others/directed-bipartite-graph.cpp
@@ -6,7 +6,7 @@
 #include <numeric>
 using namespace std;
 
-int C,D,V;
+int C,D,V,S,T;
 
 #define MAXV 500
 #define DOG (100)
@@ -17,15 +17,57 @@ int C,D,V;
 int res;
 
 int graph[200][200];
-int comb[200][200];
-int indegree[200], outdegree[200];
+int flow[256][256];
+int indegree[200], outdegree[200], level[256];
 
+void addedge(int u,int v,int a,int b) {
+    flow[u][v] += a;
+    flow[v][u] += a;
+}
+
+int bfs() {
+    fill(level, level+C+D+2, -1);
+    deque<int> Q;
+    level[S] = 0;
+    Q.push_back(S);
+    while(Q.size()) {
+        int x=Q.front(); Q.pop_front();
+        for (int v=0; v<=T; v++) if(level[v]==-1 && flow[x][v]) {
+            level[v] = level[x]+1;
+            Q.push_back(v);
+        }
+    }
+    return level[T] != -1;
+}
+
+int augmenting(int x,int w) {
+    int r=0,t;
+    if (x == T) return w;
+    for (int v=0;v<=T;v++) if (flow[x][v] && level[v]==level[x]+1) {
+        t = flow[x][v];
+        if (t + r > w) t = w - r;
+        t = augmenting(v, t);
+        r += t;
+        flow[x][v] -= t;
+        flow[v][x] += t;
+    }
+    if (! r) level[x] = -1;
+    return r;
+}
+
+int dinic() {
+    int r=0,t;
+    while (bfs()) {
+        while ((t=augmenting(S, 0x7fffffff))) r += t;
+    };
+    return r;
+}
 
 int main() {
-    int T;
-    cin>>T; 
+    int kase;
+    cin>>kase; 
     // scanf("%d", &T);
-    while (T--) {
+    while (kase--) {
         cin>>C>>D>>V;
        // scanf("%d %d %d", &C,&D,&V);
         memset(graph, 0, sizeof graph);
@@ -52,59 +94,42 @@ int main() {
             else dogLovers+=1;
         }
         //printf("#cat: %d  dog: %d\n", catLovers, dogLovers);
-        res = min(catLovers, dogLovers); // number of unsatisfy
-        int remove = 0;
-        do {
-            memset(comb, 0, sizeof comb);
-            int rs, rd, maxcomb = 0;
+        // res = min(catLovers, dogLovers); // number of unsatisfy
+        // int remove = 0;
+        {
+            memset(flow, 0, sizeof flow);
+            S = C+D;
+            T = S+1;
             for (int c=0; c<C; c++) {
                 //cout<<"Cat: "<<c+1<<" degree: "<<indegree[c]<<","<<outdegree[c]<<endl;
-                if (indegree[c]>0 && outdegree[c]>0) {
                     for (int d=0;d<D;d++) {
                         int dogid = d + DOG;
-                        if (graph[c][dogid]) {
-                            comb[c][dogid] += indegree[c];
-                            if (comb[c][dogid] > maxcomb) {
-                                rs = c; rd = dogid; maxcomb = comb[c][dogid];
-                            }
-                        }
-                        if (graph[dogid][c]) {
-                            comb[dogid][c] += outdegree[c];
-                            if (comb[dogid][c] > maxcomb) {
-                                rs = dogid; rd = c; maxcomb = comb[dogid][c];
-                            }
+                        int w = graph[c][dogid]; 
+                        if (w) {
+                            int v = C + d;
+                            addedge(c,v,w,w);
+                            addedge(S,c,w,0);
+                            addedge(S,v,w,0);
                         }
                     }
-                }
             }
 
             for (int d=0; d<D; d++) {
                 int dogid = d + DOG;
-                if (indegree[dogid]>0 && outdegree[dogid]>0) {
                     for (int c=0;c<C;c++) {
-                        if (graph[c][dogid]) {
-                            comb[c][dogid] += outdegree[c];
-                            if (comb[c][dogid] > maxcomb) {
-                                rs = c; rd = dogid; maxcomb = comb[c][dogid];
-                            }
-                        }
-                        if (graph[dogid][c]) {
-                            comb[dogid][c] += indegree[c];
-                            if (comb[dogid][c] > maxcomb) {
-                                rs = dogid; rd = c; maxcomb = comb[dogid][c];
-                            }
+
+                        int w = graph[dogid][c];
+                        if (w) {
+                            int u = C+d;
+                            addedge(u,c,w,w);
+                            addedge(u,T,w,0);
+                            addedge(c,T,w,0);
                         }
                     }
-                }
             }
-            //cout<<"#maxcomb " << maxcomb<<endl;
-            if (maxcomb==0) break;
-            remove += graph[rs][rd];
-            outdegree[rs] -= graph[rs][rd]; 
-            indegree[rd] -= graph[rs][rd]; 
-            graph[rs][rd] = 0;
-        } while (1);
-        res = min(res, remove);
-        cout<<(V - res)<<endl;
+        }
+        
+        res = dinic();
+        cout<<(V - res/2)<<endl;
     }
 }
