fcc059a4c61284e3e6f1808a26b691d0f290252a
ruqli <ruqli@microsoft.com>
2016-04-08 11:04:26 +0800

    Add threeSum

--- Library/number.cpp
+++ Library/number.cpp
@@ -1,6 +1,9 @@
 // http://rosettacode.org/wiki/Roman_numerals
 
 #include <string>
+#include <vector>
+
+using namespace std;
 
 namespace number {
     std::string int_to_roman(int value)
@@ -59,6 +62,85 @@ namespace number {
         }
         return retval;
     }
+    
+    vector< vector<int> > unique_three_sum(vector<int>& nums, int dest) {
+        sort(nums.begin(), nums.end());
+        vector< pair<int,int> > cnted;
+        vector< vector<int> > res;
+        if (nums.size() == 0) {
+            return res;
+        }
+        cnted.push_back(make_pair(nums[0],1));
+        for (int i=1; i<nums.size(); i++) {
+            pair<int,int> & top = cnted[cnted.size() - 1];
+            if (top.first == nums[i]) {
+                top.second += 1;
+            } else {
+                cnted.push_back(make_pair(nums[i],1));
+            }
+        }
+        
+        for (int ci=cnted.size() - 1; ci >=0; ci--) {
+            pair<int,int> cp = cnted[ci];
+            if (cp.first * 3 < dest) {
+                break;
+            }
+            if (cp.first * 3 == dest) {
+                if (cp.second >= 3) {
+                    vector<int> can;
+                    can.push_back(cp.first);
+                    can.push_back(cp.first);
+                    can.push_back(cp.first);
+                    res.push_back(can);
+                }
+                break;
+            }
+            
+            int sum = cp.first * 2;
+            if (cp.second == 2) {
+                for (int ai=ci - 1; ai >= 0; ai--) {
+                    if (sum + cnted[ai].first == dest) {
+                        vector<int> can;
+                        can.push_back(cnted[ai].first);
+                        can.push_back(cp.first);
+                        can.push_back(cp.first);
+                        res.push_back(can);
+                        break;
+                    }
+                }
+            }
+            sum = cp.first;
+            int bdest = dest - sum;
+            for (int bi = ci - 1; bi >= 0; bi --) {
+                pair<int,int> bp = cnted[bi];
+                if (bp.first * 2 < bdest) {
+                    break;
+                }
+                if (bp.first * 2 == dest) {
+                    if (bp.second >= 2) {
+                        vector<int> can;
+                        can.push_back(bp.first);
+                        can.push_back(bp.first);
+                        can.push_back(cp.first);
+                        res.push_back(can);
+                    }
+                    break;
+                }
+                int cdest = bdest - bp.first;
+                for (int ai=bi - 1; ai >= 0; ai--) {
+                    if (cnted[ai].first == cdest) {
+                        vector<int> can;
+                        can.push_back(cnted[ai].first);
+                        can.push_back(bp.first);
+                        can.push_back(cp.first);
+                        res.push_back(can);
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
 };
 
 class Solution {
@@ -68,5 +150,9 @@ public:
     }
     int romanToInt(string s) {
         return number::roman_to_int(s);
+    }
+    
+    vector<vector<int> > threeSum(vector<int>& nums) {
+        return number::unique_three_sum(nums, 0);
     }
 };
