608afded643bc3a2e6bfe52b60300c6581d3794d
ruqli <ruqli@microsoft.com>
2016-04-13 21:38:42 +0800

    Update namespace strings, add Valid Parentheses

--- Library/strings.cpp
+++ Library/strings.cpp
@@ -1,7 +1,12 @@
 
 #include <string>
+#include <numeric>
 
+using namespace std;
+
+#define STRINGS_CHARSET_SZ 256
 namespace strings {
+    
     std::string longest_common_prefix(std::string f, std::string s) {
         int i=0;
         for (; i<f.length() && i<s.length() && f[i]==s[i]; i++);
@@ -16,11 +21,37 @@ namespace strings {
             result = longest_common_prefix(result, strs[i]);
         return result;
     }
+    
+    bool is_closed_parentheses(string s, char *mp) {
+        stack<char> stk;
+        for(char ch : s) {
+            if (mp[ch] > 0) {
+                if (stk.size() > 0 && mp[ch]  == stk.top()) {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stk.push(ch);
+            }
+        }
+        
+        return (stk.size() == 0);
+    }
 };
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         return strings::longest_common_prefix(strs);
+    }
+    bool isValid(string s) {
+        char mp[STRINGS_CHARSET_SZ];
+        memset(mp, 0, sizeof(mp));
+        mp[')'] = '(';
+        mp[']'] = '[';
+        mp['}'] = '{';
+
+        return strings::is_closed_parentheses(s, mp);   
     }
 };
--- OSX/topcoder/topcoder/main.cpp
+++ OSX/topcoder/topcoder/main.cpp
@@ -6,6 +6,7 @@
 #include <set>
 #include <iostream>
 #include <numeric>
+#include <stack>
 #include "math.h"
 
 using namespace std;
@@ -373,6 +374,42 @@ namespace lists {
 };
 
 
+#define STRINGS_CHARSET_SZ 256
+namespace strings {
+    
+    std::string longest_common_prefix(std::string f, std::string s) {
+        int i=0;
+        for (; i<f.length() && i<s.length() && f[i]==s[i]; i++);
+        return f.substr(0, i);
+    }
+    
+    std::string longest_common_prefix(vector<std::string>& strs) {
+        int i=1;
+        if (strs.size() == 0) return "";
+        std::string result = strs[0];
+        for (; i<strs.size(); i++)
+            result = longest_common_prefix(result, strs[i]);
+        return result;
+    }
+    
+    bool is_closed_parentheses(string s, char *mp) {
+        stack<char> stk;
+        for(char ch : s) {
+            if (mp[ch] > 0) {
+                if (stk.size() > 0 && mp[ch]  == stk.top()) {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stk.push(ch);
+            }
+        }
+        
+        return (stk.size() == 0);
+    }
+};
+
 struct ExploringNumbers {
     int numberOfSteps(int n) {
         set<int> numbers;
@@ -465,6 +502,18 @@ public:
         return head;
     }
 
+    string longestCommonPrefix(vector<string>& strs) {
+        return strings::longest_common_prefix(strs);
+    }
+    bool isValid(string s) {
+        char mp[STRINGS_CHARSET_SZ];
+        memset(mp, 0, sizeof(mp));
+        mp[')'] = '(';
+        mp[']'] = '[';
+        mp['}'] = '{';
+        
+        return strings::is_closed_parentheses(s, mp);
+    }
 };
 
 struct Similars { // TCO 2015 Round 1A DIV 1
@@ -475,7 +524,10 @@ struct Similars { // TCO 2015 Round 1A DIV 1
 
 int main() {
     Solution s;
-
+    {
+        cout<< s.isValid("()[]{}") << endl;
+        return 0;
+    }
     {
         ListNode n1(1);
         ListNode n2(2);
