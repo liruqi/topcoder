72030c89bdc37f2899f8c57630190a973bc9adec
ruqli <ruqli@microsoft.com>
2016-04-15 14:30:13 +0800

    Add trie

--- Library/trie.cpp
+++ Library/trie.cpp
@@ -1,37 +1,37 @@
 #include <vector>
 #include <cstdlib>
 #include <cstdio>
-#include <cstring>
- 
+#include <string>
+#include <numeric>
+
 #define ALPHABETS 26
-#define MAX_WORD_SIZE 20
- 
+
 using namespace std;
  
-class TrieTreeNode
+class TrieNode
 {
     public:
-        TrieTreeNode * parent;
-        TrieTreeNode * children[ALPHABETS];
+        TrieNode * parent;
+        TrieNode * children[ALPHABETS];
         vector<int> occurrences;
 };
  
-class TrieTree
+class Trie
 {
     public:     
-        TrieTreeNode * root;
+        TrieNode * root;
          
-        TrieTree() {
-            root = (TrieTreeNode *) calloc(1, sizeof(TrieTreeNode));
+        Trie() {
+            root = (TrieNode *) calloc(1, sizeof(TrieNode));
         }
          
         // Inserts a word 'text' into the Trie Tree
         // 'trie_tree' and marks it's occurence as 'index'.
-        void insert(char* text, int occurrence)
+        TrieNode * insert(char* text, int occurrence)
         {
             // Converting the input word 'text'
             // into a vector for easy processing
-            TrieTreeNode * temp = root;
+            TrieNode * temp = root;
             
             while (*text) {
                 int idx = *text - 'a';
@@ -40,18 +40,42 @@ class TrieTree
           
                     // Allocate using calloc(), so that components are initialised
                     temp->children[idx] =
-                            (TrieTreeNode *) calloc(1, sizeof(TrieTreeNode));
+                            (TrieNode *) calloc(1, sizeof(TrieNode));
                     temp->children[idx]->parent = temp; // Assigning parent
                 }
           
                 temp = temp->children[idx];
             }
             temp->occurrences.push_back(occurrence);      //Mark the occurrence of the word
+            return temp;
         }
-    
+
+        TrieNode * insert(string text, int occurrence)
+        {
+            // Converting the input word 'text'
+            // into a vector for easy processing
+            TrieNode * temp = root;
+            
+            for (char ch : text) {
+                int idx = ch - 'a';
+                if (temp->children[idx] == NULL) {
+                    // There is no node in 'trie_tree' corresponding to this alphabet
+          
+                    // Allocate using calloc(), so that components are initialised
+                    temp->children[idx] =
+                            (TrieNode *) calloc(1, sizeof(TrieNode));
+                    temp->children[idx]->parent = temp; // Assigning parent
+                }
+          
+                temp = temp->children[idx];
+            }
+            temp->occurrences.push_back(occurrence);      //Mark the occurrence of the word
+            return temp;
+        }
+
         // Prints the 'trie_tree' in a Pre-Order or a DFS manner
         // which automatically results in a Lexicographical Order
-        void lexicographPrint(TrieTreeNode * trie_tree, vector<char> printUtilVector)
+        void lexicographPrint(TrieNode * trie_tree, vector<char> printUtilVector)
         {
             int i;
             bool noChild = true;
@@ -87,7 +111,6 @@ class TrieTree
                 printf(" -> @ index -> ");
           
                 vector<int>::iterator counter = trie_tree->occurrences.begin();
-                // This is to print the occurences of the word
           
                 while (counter != trie_tree->occurrences.end()) {
                     printf("%d, ", *counter);
@@ -104,10 +127,10 @@ class TrieTree
         // if not found, returns NULL,
         // if found, returns poniter pointing to the
         // last node of the word in the 'trie_tree'
-        TrieTreeNode * searchWord(char * text)
+        TrieNode * searchWord(char * text)
         {
             // Functions very similar to insert() function
-            TrieTreeNode * temp = root;
+            TrieNode * temp = root;
           
             while (* text) {
                 int idx = *text - 'a';
@@ -124,7 +147,7 @@ class TrieTree
         // if found, deletes the nodes corresponding to the word
         void removeWord(char * word)
         {
-            TrieTreeNode * temp = searchWord(word);
+            TrieNode * temp = searchWord(word);
           
             if (temp == NULL) {
                 // Word not found
@@ -160,7 +183,67 @@ class TrieTree
         }
 };
 
+class Solution {
+public:
+    vector<int> cnt;
+    vector<int> findSubstring(string s, vector<string>& words) {
+        Trie trie;
+        int id = 0;
+        cnt.clear();
+        for (string w : words) {
+            TrieNode *nd = trie.insert(w, id);
+            id ++;
+            cnt.push_back(0);
+            cnt[nd->occurrences[0]] += 1;
+        }
+        vector<int> res;
+        return dfs(s, trie);
+    }
+    
+    vector<int> dfs(string s, Trie trie) {
+        // check if substr s can match with start position
+        vector<int> rcnt = cnt;
+        vector<int> res;
+        if (findMatch(s, trie, rcnt)) {
+            res.push_back(0);
+        }
+        if (s.size() > 0) {
+            vector<int> more = dfs(s.substr(1), trie);
+            for (int idx : more) {
+                res.push_back(idx + 1);
+            }
+        }
+        return res;
+    }
+    
+    bool findMatch(string s, Trie trie, vector<int> & rcnt) {
+        TrieNode *it = trie.root;
+        for (int i=0; i<s.size(); i++) {
+            char ch = s[i];
+            it = it->children[ch - 'a'];
+            if (! it) {
+                break;
+            }
+            if (it->occurrences.size() == 0) {
+                continue;
+            }
+            int wordIndex = it->occurrences[0];
+            if (rcnt[wordIndex] > 0) {
+                rcnt[wordIndex] -= 1;
+                if (findMatch(s.substr(i + 1), trie, rcnt)) {
+                    return true;
+                }
+                rcnt[wordIndex] += 1;
+            }
+        }
+        return (accumulate(rcnt.begin(), rcnt.end(), 0) == 0);
+    }
+};
+
 int main ()
 {
+    Solution s;
+    vector<string> words{"foo", "bar"};
+    auto ans = s.findSubstring("barfoothefoobarman", words);
     return 0;
 }
--- OSX/topcoder/topcoder/main.cpp
+++ OSX/topcoder/topcoder/main.cpp
@@ -9,6 +9,7 @@
 #include <stack>
 #include <queue>
 #include "math.h"
+#include "assert.h"
 
 using namespace std;
 
@@ -315,6 +316,11 @@ namespace numbers {
         }
         return 0;
     }
+    size_t dump(vector<int> lst) {
+        for (int v : lst) { cout<<v<<" "; }
+        cout<<" # len="<<lst.size()<<endl;
+        return lst.size();
+    }
 };
 
 namespace maths {
@@ -609,6 +615,187 @@ struct ReplacingDigit {
 };
 
 
+
+#define ALPHABETS 26
+
+using namespace std;
+
+class TrieNode
+{
+public:
+    TrieNode * parent;
+    TrieNode * children[ALPHABETS];
+    vector<int> occurrences;
+};
+
+class Trie
+{
+public:
+    TrieNode * root;
+    
+    Trie() {
+        root = (TrieNode *) calloc(1, sizeof(TrieNode));
+    }
+    
+    // Inserts a word 'text' into the Trie Tree
+    // 'trie_tree' and marks it's occurence as 'index'.
+    TrieNode * insert(char* text, int occurrence)
+    {
+        // Converting the input word 'text'
+        // into a vector for easy processing
+        TrieNode * temp = root;
+        
+        while (*text) {
+            int idx = *text - 'a';
+            if (temp->children[idx] == NULL) {
+                // There is no node in 'trie_tree' corresponding to this alphabet
+                
+                // Allocate using calloc(), so that components are initialised
+                temp->children[idx] =
+                (TrieNode *) calloc(1, sizeof(TrieNode));
+                temp->children[idx]->parent = temp; // Assigning parent
+            }
+            
+            temp = temp->children[idx];
+        }
+        temp->occurrences.push_back(occurrence);      //Mark the occurrence of the word
+        return temp;
+    }
+    
+    TrieNode * insert(string text, int occurrence)
+    {
+        // Converting the input word 'text'
+        // into a vector for easy processing
+        TrieNode * temp = root;
+        
+        for (char ch : text) {
+            int idx = ch - 'a';
+            if (temp->children[idx] == NULL) {
+                // There is no node in 'trie_tree' corresponding to this alphabet
+                
+                // Allocate using calloc(), so that components are initialised
+                temp->children[idx] =
+                (TrieNode *) calloc(1, sizeof(TrieNode));
+                temp->children[idx]->parent = temp; // Assigning parent
+            }
+            
+            temp = temp->children[idx];
+        }
+        temp->occurrences.push_back(occurrence);      //Mark the occurrence of the word
+        return temp;
+    }
+    
+    // Prints the 'trie_tree' in a Pre-Order or a DFS manner
+    // which automatically results in a Lexicographical Order
+    void lexicographPrint(TrieNode * trie_tree, vector<char> printUtilVector)
+    {
+        int i;
+        bool noChild = true;
+        
+        vector<int>::iterator itr = trie_tree->occurrences.begin();
+        
+        for (i = 0; i < ALPHABETS; ++i) {
+            if (trie_tree->children[i] == NULL) {
+                continue;
+            } else {
+                noChild = false;
+                printUtilVector.push_back('a' + i);    // Select a child
+                
+                // and explore everything associated with the cild
+                lexicographPrint(trie_tree->children[i], printUtilVector);
+                printUtilVector.pop_back();
+                // Remove the alphabet as we dealt
+                // everything associated with it
+            }
+        }
+        
+        if (trie_tree->occurrences.size() != 0) {
+            // Condition trie_tree->occurrences.size() != 0,
+            // is a neccessary and sufficient condition to
+            // tell if a node is associated with a word or not
+            
+            vector<char>::iterator itr = printUtilVector.begin();
+            
+            while (itr != printUtilVector.end()) {
+                printf("%c", *itr);
+                ++itr;
+            }
+            printf(" -> @ index -> ");
+            
+            vector<int>::iterator counter = trie_tree->occurrences.begin();
+            // This is to print the occurences of the word
+            
+            while (counter != trie_tree->occurrences.end()) {
+                printf("%d, ", *counter);
+                ++counter;
+            }
+            
+            printf("\n");
+        }
+        
+        printUtilVector.pop_back();
+    }
+    
+    // Searches for the occurence of a word in 'trie_tree',
+    // if not found, returns NULL,
+    // if found, returns poniter pointing to the
+    // last node of the word in the 'trie_tree'
+    TrieNode * searchWord(char * text)
+    {
+        // Functions very similar to insert() function
+        TrieNode * temp = root;
+        
+        while (* text) {
+            int idx = *text - 'a';
+            if (temp->children[idx]) {
+                temp = temp->children[idx];
+            } else {
+                return NULL;
+            }
+        }
+        return (* text) ? NULL : temp;
+    }
+    
+    // Searches the word first, if not found, does nothing
+    // if found, deletes the nodes corresponding to the word
+    void removeWord(char * word)
+    {
+        TrieNode * temp = searchWord(word);
+        
+        if (temp == NULL) {
+            // Word not found
+            return;
+        }
+        
+        temp->occurrences.pop_back();    // Deleting the occurence
+        
+        // 'noChild' indicates if the node is a leaf node
+        
+        int childCount = 0;
+        // 'childCount' has the number of children the current node
+        // has which actually tells us if the node is associated with
+        // another word .This will happen if 'childCount' >= 2.
+        int i;
+        
+        // Checking children of current node
+        for (i = 0; i < ALPHABETS; ++i) {
+            if (temp->children[i] != NULL) {
+                ++childCount;
+            }
+        }
+        
+        if (childCount > 0) {
+            // The node has children, which means that the word whose
+            // occurrence was just removed is a Suffix-Word
+            // So no more nodes have to be deleted
+            return;
+        }
+        
+        // todo: free memory,
+        // reserve allocated memory for further use.
+    }
+};
+
 class Solution {
 public:
     string intToRoman(int num) {
@@ -686,7 +873,59 @@ public:
     string convertToTitle(int n) {
         return strings::long_to_excel_column(n);
     }
-
+    vector<int> cnt;
+    vector<int> findSubstring(string s, vector<string>& words) {
+        Trie trie;
+        int id = 0;
+        cnt.clear();
+        for (string w : words) {
+            TrieNode *nd = trie.insert(w, id);
+            id ++;
+            cnt.push_back(0);
+            cnt[nd->occurrences[0]] += 1;
+        }
+        vector<int> res;
+        return dfs(s, trie);
+    }
+    
+    vector<int> dfs(string s, Trie trie) {
+        // check if substr s can match with start position
+        vector<int> rcnt = cnt;
+        vector<int> res;
+        if (findMatch(s, trie, rcnt)) {
+            res.push_back(0);
+        }
+        if (s.size() > 0) {
+            vector<int> more = dfs(s.substr(1), trie);
+            for (int idx : more) {
+                res.push_back(idx + 1);
+            }
+        }
+        return res;
+    }
+    
+    bool findMatch(string s, Trie trie, vector<int> & rcnt) {
+        TrieNode *it = trie.root;
+        for (int i=0; i<s.size(); i++) {
+            char ch = s[i];
+            it = it->children[ch - 'a'];
+            if (! it) {
+                break;
+            }
+            if (it->occurrences.size() == 0) {
+                continue;
+            }
+            int wordIndex = it->occurrences[0];
+            if (rcnt[wordIndex] > 0) {
+                rcnt[wordIndex] -= 1;
+                if (findMatch(s.substr(i + 1), trie, rcnt)) {
+                    return true;
+                }
+                rcnt[wordIndex] += 1;
+            }
+        }
+        return (accumulate(rcnt.begin(), rcnt.end(), 0) == 0);
+    }
 };
 
 struct Similars { // TCO 2015 Round 1A DIV 1
@@ -697,6 +936,11 @@ struct Similars { // TCO 2015 Round 1A DIV 1
 
 int main() {
     Solution s;
+    {
+        vector<string> words{"foo", "bar"};
+        vector<int> ans = s.findSubstring("barfoothefoobarman", words);
+        numbers::dump(ans);
+    }
     {
         cout << s.convertToTitle(702) << endl;
         return 0;
