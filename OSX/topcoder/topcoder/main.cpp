a3e6f8e32e4f05b02ca0522da96329f8a2fde9dd
ruqli <ruqli@microsoft.com>
2016-04-18 20:29:18 +0800

    Add solution for Subsets, leetcode.com/problems/subsets/

--- OSX/topcoder/topcoder/main.cpp
+++ OSX/topcoder/topcoder/main.cpp
@@ -1082,6 +1082,20 @@ public:
         }
         return numbers::_unique_sum_fixed_size(cnted, 0, target, k);
     }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        long mask;
+        long upper = 1 << nums.size();
+        size_t n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int> > ans;
+        for (mask=0; mask < upper; mask ++) {
+            vector<int> can;
+            for(int b=0; b<n; b++) if((1<<b) & mask) can.push_back(nums[b]);
+            ans.push_back(can);
+        }
+        return ans;
+    }
 };
 
 struct Similars { // TCO 2015 Round 1A DIV 1
