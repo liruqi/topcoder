4046ee11ca3df8b8caa70f6b6b2836976cdc28d7
ruqli <ruqli@microsoft.com>
2016-04-20 01:19:14 +0800

    😀 Maximum Subarray

--- OSX/topcoder/topcoder/main.cpp
+++ OSX/topcoder/topcoder/main.cpp
@@ -1146,6 +1146,30 @@ public:
         }
         return sum;
     }
+    
+    int maxSubArray(vector<int>& nums) {
+        vector<int> sums;
+        
+        int s = 0;
+        for(int i=0; i<nums.size(); i++) {
+            sums.push_back(s);
+            s+=nums[i];
+        }
+        sums.push_back(s);
+        vector<int> maxAfterIndex(nums.size());
+        int mval = sums[sums.size() - 1];
+        int ans = mval;
+        
+        for (int i=sums.size() - 2; i>=0; i--) {
+            maxAfterIndex[i] = mval;
+            if (sums[i] > mval) mval = sums[i];
+        }
+        for (int i=0; i<maxAfterIndex.size(); i++) {
+            int can = maxAfterIndex[i] - sums[i];
+            if (can > ans) ans = can;
+        }
+        return ans;
+    }
 };
 
 struct Similars { // TCO 2015 Round 1A DIV 1
@@ -1157,10 +1181,10 @@ struct Similars { // TCO 2015 Round 1A DIV 1
 int main() {
     Solution s;
     {
-        vector<int > D{4, 5, 6, 7, 0, 1, 2};
-        
+        vector<int > D{0};
+        int p = s.maxSubArray(D);
 
-        int p = s.search(D, 2);
+        p = s.search(D, 2);
         cout << p << endl;
     }
     {
