d676f707185adf7e4db601119ce028d77f365c54
ruqli <ruqli@microsoft.com>
2016-04-19 15:26:46 +0800

    😀 Solved First Missing Positive

--- OSX/topcoder/topcoder/main.cpp
+++ OSX/topcoder/topcoder/main.cpp
@@ -1096,6 +1096,24 @@ public:
         }
         return ans;
     }
+    
+    int firstMissingPositive(vector<int>& nums) {
+        size_t sz = nums.size();
+        vector<int> cnt(nums.size(), 0);
+        for (int v : nums) {
+            if (v > 0) {
+                if (v <= sz) {
+                    cnt[v - 1] += 1;
+                }
+            }
+        }
+        for (int i = 0; i<sz; i++) {
+            if (cnt[i] == 0) {
+                return i + 1;
+            }
+        }
+        return sz + 1;
+    }
 };
 
 struct Similars { // TCO 2015 Round 1A DIV 1
