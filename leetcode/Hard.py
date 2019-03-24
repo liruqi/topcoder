9783287ffaef744e4782c6b5f49da4dde975b003
Ruqi <liruqi@gmail.com>
2019-03-24 18:56:43 +0800

    Add largest-rectangle-in-histogram

--- leetcode/Hard.py
+++ leetcode/Hard.py
@@ -54,3 +54,47 @@ class Solution:
             return max(mds + root.val, root.val)
         return mds + root.val
     # End of binary-tree-maximum-path-sum
+    
+    # https://leetcode.com/problems/largest-rectangle-in-histogram/
+    def largestRectangleArea(self, heights) -> int:
+        N = len(heights)
+        lIdx, incStack = self.buildIncIndex(heights)
+        lIdx = [-1 if x>=N else x for x in lIdx]
+        heights.reverse()
+        rrIdx, decStack = self.buildIncIndex(heights)
+        print (rrIdx, heights)
+
+        rrIdx.reverse()
+        rIdx = [N - 1 - x if x>=0 else x for x in rrIdx]
+        print (lIdx, rIdx)
+        heights.reverse()
+        can = 0
+        for i in range(N):
+            x = heights[i]
+            width = 1
+            if (lIdx[i] >= 0):
+                width += (i - lIdx[i])
+            if (rIdx[i] > i):
+                width += (rIdx[i] - i)
+            can = max(can, width * x)
+        return can
+
+    def buildIncIndex(self, heights):
+        leftIdx, incStack = [], []
+        N = len(heights)
+
+        for i in range(len(heights)):
+            x = heights[i]
+            idx = N
+            while incStack and x <= heights[incStack[-1]]:
+                idx = incStack[-1]
+                incStack.pop()
+            if (idx < N and leftIdx[idx]<N):
+                idx = leftIdx[idx]
+            incStack.append(i)
+            leftIdx.append(idx)
+        return (leftIdx, incStack)
+
+    def largestRectangleAreaTest(self):
+        print (self.largestRectangleArea([2,1,5,6,2,3]))
+    # End of largest-rectangle-in-histogram
