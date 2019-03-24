93796c04fd65716ca8402e5ed9d912b6d941c616
ruqli <ruqli@outlook.com>
2019-03-24 20:00:18 +0800

    Add https://leetcode.com/problems/maximal-rectangle/

--- leetcode/Hard.py
+++ leetcode/Hard.py
@@ -98,3 +98,34 @@ class Solution:
     def largestRectangleAreaTest(self):
         print (self.largestRectangleArea([2,1,5,6,2,3]))
     # End of largest-rectangle-in-histogram
+
+    # https://leetcode.com/problems/maximal-rectangle/
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        mtx = []
+        for row in matrix:
+            c = 0
+            drow = []
+            for x in row:
+                if x == "0":
+                    c = 0
+                    drow.append(0)
+                else:
+                    c+=1
+                    drow.append(c)
+            mtx.append(drow)
+        can = 0
+        for i in range(len(mtx)):
+            row = mtx[i]
+            for j in range(len(row)):
+                k = i
+                area = 0
+                minr = row[j]
+                while (minr > 0):
+                    can = max(can,
+                              minr * (1 + i - k))
+                    k -= 1
+                    if (k < 0):
+                        break
+                    minr = min(minr, mtx[k][j])
+        return can
+    # End of maximal-rectangle
