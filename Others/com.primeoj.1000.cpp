04947828f625a8750889691c6c9d378f58dad72f
liruqi <liruqi@gmail.com>
2023-12-28 18:17:20 +0700

    RE20

--- Others/com.primeoj.1000.cpp
+++ Others/com.primeoj.1000.cpp
@@ -1,63 +1,100 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <algorithm>
+#include <iostream>
+#include <cmath>
 #include <vector>
-#include <bitset>
 
-#define MAX_N 2038074743
-#define IN_N 10
-#define STK_PRIME_SIZE 100000000
+using namespace std;
 
+const unsigned int MAX_N = 2038074743;
+const unsigned int IN_N = 10;
+const unsigned int SQRT_N = 65536; // math.sqrt(2038074743) -> 45145.041178406296
 unsigned int inp[IN_N];
+unsigned int limit = sqrt(MAX_N);
+vector<unsigned int> primes;
+vector<unsigned int> prime_states;
+bool is_prime[SQRT_N];
 
-std::bitset<MAX_N> not_prime;
-unsigned int* stk_prime;
-void sieve_of_eratosthenes(int maxInp) {
-    // 初始化标志数组，假设所有数都是素数
-    unsigned long i = 2;
-    not_prime.set(2, false);
-    stk_prime = new unsigned int[maxInp+1];
-    stk_prime[0] = 0;
-    // 使用埃拉托斯特尼筛法，将非素数的标志位设为0
-    for (i = 2; i*i < MAX_N; ++i) {
-        if (!not_prime[i]) {
-            stk_prime[0]+=1;
-            stk_prime[stk_prime[0]] = i;
-            if (stk_prime[0] >= maxInp) break;
-            for (unsigned long j = i * i; j < MAX_N; j += i) {
-                not_prime[j] = true;
+int simple_sieve(unsigned int limit) {
+    fill(is_prime, is_prime + limit, true);
+    for (unsigned int num = 2; num < limit; ++num) {
+        if (is_prime[num]) {
+            primes.push_back(num);
+            unsigned int multiple = num * num;
+            while (multiple < limit) {
+                is_prime[multiple] = false;
+                multiple += num;
             }
+            prime_states.push_back(multiple);
         }
     }
-    for (; i<MAX_N && stk_prime[0]<maxInp; ++i) {
-        if (!not_prime[i]) {
-            stk_prime[0]+=1;
-            stk_prime[stk_prime[0]] = i;
-        }
-    }
+    return 0;
 }
 
-int main() {
-	int q = -1;
-    char line[100];
-	int i;
+unsigned int sieve_of_eratosthenes(unsigned int maxInp) {
+    simple_sieve(limit);
+
+    unsigned int low = limit;
+    unsigned int high = 2 * limit;
+
+    while (low < MAX_N) {
+        bool is_prime[limit];
+        fill(is_prime, is_prime + limit, true);
+        if (high > MAX_N) {
+            high = MAX_N;
+        }
+
+        for (size_t pos = 0; pos < prime_states.size(); ++pos) {
+            unsigned int prime = primes[pos];
+            unsigned int multiple = prime_states[pos];
+            while (multiple < high) {
+                is_prime[multiple - low] = false;
+                multiple += prime;
+            }
+            prime_states[pos] = multiple;
+        }
+
+        // Store new primes found in the current block
+        for (unsigned int i = low; i < high; ++i) {
+            if (is_prime[i - low]) {
+                primes.push_back(i);
+            }
+        }
+
+        if (primes.size() >= static_cast<size_t>(maxInp)) {
+            return primes.size();
+        }
+
+        low += limit;
+        high += limit;
+    }
+
+    return primes.size();
+}
+
+unsigned int main() {
+    unsigned int q = -1;
     unsigned int maxInp = 0;
-	for (i = 0; i < IN_N; i++) {
-		scanf("%s\n", line);
-		if (line[0] != 'q')
-			sscanf(line, "%u", &inp[i]);
-		else {
-			sscanf(line, "qq_group:%u", &inp[i]);
-			q = i;
-		}
-		maxInp = std::max(maxInp, inp[i]);
-	}
-    // 调用埃拉托斯特尼筛法函数
-    sieve_of_eratosthenes(maxInp);
-    for (i = 0; i < IN_N; i++) {
-		if (i == q)
-			printf("qq_group:");
-		printf("%u\n", stk_prime[inp[i]]);
-	}
+
+    for (unsigned int i = 0; i < IN_N; ++i) {
+        string line;
+        getline(cin, line);
+        if (line[0] != 'q') {
+            inp[i] = stoi(line);
+        } else {
+            inp[i] = stoi(line.substr(9));
+            q = i;
+        }
+        maxInp = max(maxInp, inp[i]);
+    }
+
+    // Call the sieve_of_eratosthenes function
+    unsigned int prime_number = sieve_of_eratosthenes(maxInp);
+
+    for (unsigned int i = 0; i < IN_N; ++i) {
+        if (i == q) {
+            cout << "qq_group:";
+        }
+        cout << primes[inp[i] - 1] << endl;
+    }
+
     return 0;
 }
